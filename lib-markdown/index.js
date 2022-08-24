b9313fcc87c06d2d17dc225cea5957987e8f14d0
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-24 16:53:34 -0300

    Correção do erro ES module

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,1 +1,11 @@
-console.log("Primeiro Console")
+import chalk from 'chalk';
+
+console.log(chalk.blue.bgRed.bold('Primeiro Console'));
+
+const paragrafo = 'Texto do meu paragrafo.';
+
+function texto(string) {
+    return string;
+}
+
+console.log(texto(paragrafo)); 
