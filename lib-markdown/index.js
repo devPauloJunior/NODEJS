21d2800b21ce4764132c4299fe7046342b9fc567
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-24 15:51:59 -0300

    Primeiro Console.Log

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,11 +1,1 @@
-const chalk = require('chalk');
-
-console.log(chalk.blue('vamos começar'));
-
-const  paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-    return string;
-}
-
-console.log(texto(paragrafo));
+console.log("Primeiro Console")
