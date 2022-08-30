7d0ddb0114cdca12f5ceb025dcb711b99eed52a2
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-30 12:08:15 -0300

    Tratamento de erros com throw

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,22 +1,18 @@
 import chalk from 'chalk';
 import fs from 'fs';
-//const fs = require('fs');
 
-// console.log(chalk.blue.bgRed.bold('Primeiro Console'));
-
-// const paragrafo = 'Texto do meu paragrafo.';
-
-// function texto(string) {
-//     return string;
-// }
-
-// console.log(texto(paragrafo)); 
+function TratarErro(erro) {
+    throw new Error(chalk.red(erro.code, 'verifique o camminho do arquivo especifiado!'));
+}
 
 function PegaArquivo(caminhoDoArquivo) {
     const econding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, econding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, econding, (erro, texto) => {
+        if (erro) {
+            TratarErro(erro);
+        }
         console.log(chalk.green(texto));
     })
 }
 
-PegaArquivo('./arquivos/texto1.md');
+PegaArquivo('./arquivos/texto12.md');
