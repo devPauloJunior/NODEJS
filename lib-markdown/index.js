4ece9509dac87c33cd1202fba6a45c111ccc1080
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-30 11:56:36 -0300

    A lib fs e suas funcionalidades com import fs from

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,11 +1,22 @@
 import chalk from 'chalk';
+import fs from 'fs';
+//const fs = require('fs');
 
-console.log(chalk.blue.bgRed.bold('Primeiro Console'));
+// console.log(chalk.blue.bgRed.bold('Primeiro Console'));
 
-const paragrafo = 'Texto do meu paragrafo.';
+// const paragrafo = 'Texto do meu paragrafo.';
 
-function texto(string) {
-    return string;
+// function texto(string) {
+//     return string;
+// }
+
+// console.log(texto(paragrafo)); 
+
+function PegaArquivo(caminhoDoArquivo) {
+    const econding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, econding, (_, texto) => {
+        console.log(chalk.green(texto));
+    })
 }
 
-console.log(texto(paragrafo)); 
+PegaArquivo('./arquivos/texto1.md');
